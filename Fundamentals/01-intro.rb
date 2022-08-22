228fb06790d92ed0df3d75c00b977c99e553deb9
wiliamdarmawan <generousbil@gmail.com>
2022-08-22 16:30:51 +0700

    1st Lesson

--- Fundamentals/01-intro.rb
+++ Fundamentals/01-intro.rb
@@ -1,1 +1,12 @@
-puts "Hello World"
+# print "Hello World\n"
+# puts "Hello World"
+# p "Hello World"
+
+# greeting = "Hello Wiliam"
+# puts greeting
+
+def say_hello(thing_to_say)
+    puts thing_to_say
+end
+
+say_hello "Hello World Ruby is great!"
