cf43fc37d2873db3cc1bf581bd6166bb6ef035b7
wiliamdarmawan <generousbil@gmail.com>
2022-08-23 09:07:54 +0700

    2nd Lesson - Strings

---
+++ Fundamentals/02_strings.rb
@@ -0,0 +1,12 @@
+# String concatenation
+first_name = "Wiliam"
+last_name = "Darmawan"
+puts first_name + " " + last_name
+
+# String interpolation
+puts "My first name is #{first_name} and my last name is #{last_name}"
+
+# Methods, how to find them
+    # variable_name.methods
+
+
