6a633f46e9dc47f4b420fa66fca612e84212bd12
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-08-29 10:02:25 +0700

    Numbers

---
+++ Fundamentals/04_numbers.rb
@@ -0,0 +1,24 @@
+# x = 5
+# y = 10
+# puts y / x
+
+# puts "I am a line"
+# puts "-" * 20
+# puts "I am a different line after a divider"
+# 20.times { print "-" }
+# 20.times { puts "hi" }
+# 20.times { puts rand(10) }
+
+
+puts "Simple calculator"
+25.times { print "-" }
+puts
+puts "Enter the first number"
+num_1 = gets.chomp
+puts "Enter the second number"
+num_2 = gets.chomp
+puts "The first number multiplied by the second number is #{num_1.to_f * num_2.to_f}"
+puts "The first number divided by the second number is #{num_1.to_f / num_2.to_f}"
+puts "The first number added by the second number is #{num_1.to_f + num_2.to_f}"
+puts "The first number subtracted by the second number is #{num_1.to_f - num_2.to_f}"
+puts "The first number modulus by the second number is #{num_1.to_f % num_2.to_f}"
