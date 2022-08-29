4a7b58650878d3e981d4e4eb5273286f8ac3d076
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-08-29 10:05:35 +0700

    Comparison Operators

---
+++ Fundamentals/05_comparison.rb
@@ -0,0 +1,11 @@
+# ==
+# =
+# !=
+# <
+# >
+puts 10 == 10
+puts 10 != 9
+puts "hello" != "hello"
+puts "hello" == "hello"
+puts 10 == "10".to_f
+puts 10.eql?(10.0) # -> to compare types. This will return false
