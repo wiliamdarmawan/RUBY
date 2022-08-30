b5567dd29b0f7ce326042c3f7f5f3c0c848390f4
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-08-30 11:55:06 +0700

    Style fix using rubocop

--- Exercises/01_authenticator.rb
+++ Exercises/01_authenticator.rb
@@ -1,35 +1,34 @@
 def auth_user(username, password, users)
-    users.each do |user|
-        if( user[:username] == username && user[:password] == password )
-            return user
-        end
-    end
-    "Credentials were not correct"
+  users.each do |user|
+    return user if user[:username] == username && user[:password] == password
+  end
+  'Credentials were not correct'
 end
 
 users = [
-    { username: "Wiliam", password: "password1" },
-    { username: "Darmawan", password: "password2" }
+  { username: 'Wiliam', password: 'password1' },
+  { username: 'Darmawan', password: 'password2' }
 ]
 
-puts "Welcome to the authenticator"
-25.times { print "-" }
+puts 'Welcome to the authenticator'
+25.times { print '-' }
 puts
-puts "This program will take input from the user and compare password"
-puts "If password is correct, you will get back the user object"
+puts 'This program will take input from the user and compare password'
+puts 'If password is correct, you will get back the user object'
 attempt = 0
 
 while attempt < 3
-    print "Username: "
-    username = gets.chomp
-    print "Password: "
-    password = gets.chomp
+  print 'Username: '
+  username = gets.chomp
+  print 'Password: '
+  password = gets.chomp
 
-    user = auth_user(username, password, users)
-    puts user
-    puts "Press n to quit or any other key to continue"
-    input = gets.chomp.downcase
-    break if ( input == 'n')
-    attempt += 1
+  user = auth_user(username, password, users)
+  puts user
+  puts 'Press n to quit or any other key to continue'
+  input = gets.chomp.downcase
+  break if input == 'n'
+
+  attempt += 1
 end
-puts "You have exceeded the number of attempts" if attempt >= 3
+puts 'You have exceeded the number of attempts' if attempt >= 3
