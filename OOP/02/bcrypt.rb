f539e3299c6b2be78125adbd9a51fc662bb3eb75
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-08-31 10:02:52 +0700

    OOP - Methods

--- OOP/02/bcrypt.rb
+++ OOP/02/bcrypt.rb
@@ -1,14 +1,16 @@
 require 'bcrypt'
 
 my_password = BCrypt::Password.create("my password")
-#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
+my_password_1 = BCrypt::Password.create("my password")
+my_password_2 = BCrypt::Password.create("my password")
 
 puts my_password
-puts my_password.version              #=> "2a"
-puts my_password.cost                 #=> 12
-puts my_password == "my password"     #=> true
-puts my_password == "not my password" #=> false
+puts my_password_1
+puts my_password_2
 
-# puts my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
+puts my_password == my_password_1
+puts my_password == "my password"
+
+# puts my_password = BCrypt::Password.new("$2a$12$GypGLR/K1MmXzlpP/h7UfupvKyPnR0wNULfNs3t.fNrh7A7Jy452e")
 # puts my_password == "my password"     #=> true
 # puts my_password == "not my password" #=> false
